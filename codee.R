95e9be71951eff692311b503ff957fa5d6487f66
houds <90451063+houds@users.noreply.github.com>
2022-01-25 17:19:48 +0100

    Update codee.R

--- codee.R
+++ codee.R
@@ -1,18 +1,18 @@
 
 
 # Importation des tables
+
 brute =read.csv("monfichier.csv",header = TRUE, sep= ";", encoding="UTF-8")
-saveRDS(brute, file="brute.rds")
-attach(brute)
+
+
 data=read.csv("data.csv",header = TRUE, sep= ";", encoding="UTF-8") 
-saveRDS(data, file="data.rds")
-attach(data)
+
+
 
 train=read.csv("train.csv",header = TRUE, sep= ";", encoding="UTF-8")
-saveRDS(train, file="train.rds")
+
 
 valid=read.csv("valid.csv",header = TRUE, sep= ";", encoding="UTF-8")
-saveRDS(valid, file="valid.rds")
 
 
 
